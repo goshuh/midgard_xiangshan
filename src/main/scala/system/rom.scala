0b9b8a2281e77961d8b0e3eca1952e751ae3857d
Rrraaaeee <qingxuannn0602@gmail.com>
2023-03-03 02:24:04 +0000

    Added write port to rom. Rename rom in the future

--- src/main/scala/system/rom.scala
+++ src/main/scala/system/rom.scala
@@ -12,9 +12,11 @@ import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.tilelink._
 
 class RomBlackBoxIO extends Bundle {
-  val clk = Input(Bool())
+  val clk = Input(Clock())
   val en = Input(Bool())
-  val addr = Input(UInt(16.W))
+  val we = Input(Bool())
+  val addr = Input(UInt(27.W))
+  val di  = Input(UInt(64.W))
   val dout = Output(UInt(64.W))
 }
 
@@ -43,38 +45,78 @@ class Rom(implicit p: Parameters) extends LazyModule {
     )
   )
 
+
   lazy val module = new LazyModuleImp(this) {
     val (i, ie) = node.in.head
     val rom = Module(new RomBlackBox()).io
+    val rom_dbg = IO(Output(Bool()))
+    rom_dbg  := i.a.fire
 
-    val idle :: read :: resp :: Nil = Enum(3)
+    val idle :: read :: read_resp :: write_full :: write_partial :: write_resp :: Nil = Enum(6)
     val state = RegInit(idle)
     val context = Reg(i.a.bits.cloneType) // TLbundleA
     val data = Reg(UInt(64.W))
 
+    rom.clk := clock
     rom.en := false.B
+    rom.we := false.B
+    rom.di := 0.U
     rom.addr := 0.U
+
     i.a.ready := state === idle
-    i.d.valid := state === resp
+    i.d.valid := state === read_resp || state === write_resp
 
     switch(state) {
       is(idle) {
         when(i.a.fire) {
+          val wr_full = (i.a.bits.opcode === TLMessages.PutFullData)
+          val wr_part = (i.a.bits.opcode === TLMessages.PutPartialData)
           rom.en := true.B
-          rom.addr := i.a.bits.address
+          rom.addr := i.a.bits.address(26,3)
+          rom.we := wr_full
+          rom.di := i.a.bits.data
+
           context := i.a.bits
-          state := read
+          when (wr_full) {
+            state := write_full
+          }.elsewhen (wr_part) {
+            state := write_partial
+          }.otherwise{
+            state := read
+          }
         }
       }
       is(read) {
         data := rom.dout
-        state := resp
+        state := read_resp
       }
-      is(resp) {
+      is(write_full) {
+        state := write_resp
+      }
+      is(write_partial) {
+        rom.en := true.B
+        rom.addr := context.address(26,3)
+        rom.we := true.B
+        val data = Wire(Vec(8, UInt(8.W)))
+        for (i <- 0 until 8) {
+          val sel = (context.mask >> i.U)
+          when (sel(0)) {
+            data(i) := (context.data >> (i*8).U) & (255.U)
+          }.otherwise {
+            data(i) := (rom.dout >> (i*8).U) & (255.U)
+          }
+        }
+        rom.di := data.asTypeOf(UInt(64.W))
+        state := write_resp
+      }
+      is(read_resp) {
         i.d.bits := ie.AccessAck(context, data)
         when(i.d.fire) { state := idle }
       }
-
+      is(write_resp) {
+        i.d.bits := ie.AccessAck(context)
+        when(i.d.fire) { state := idle }
+      }
     }
 
   }
